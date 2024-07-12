5572c6417acf536898b9fc83d74aafb16608828d
MahendraRajput123 <mahendra@ealphabits.com>
2024-07-12 16:10:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const cloudRoutes = require("./routes/cloudinary");
+const socket = require("socket.io");
+    
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/cloud", cloudRoutes);
+
+const PORT = process.env.PORT || 5000
+const server = app.listen(PORT,()=>{
+    console.log(`Server running on Port ${PORT}`);
+})
+
+const io = socket(server,{
+  cors :{
+    origin : '*',
+    credentials : true
+  }
+})
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket)=>{
+  console.log('connect to socket', socket.id);
+  global.chatSocket = socket;
+
+  socket.on("add-user", (userId)=>{
+    onlineUsers.set(userId, socket.id);
+  })
+
+  socket.on("send-msg", (data)=>{
+    const sendUnderSocket = onlineUsers.get(data.to);
+    if(sendUnderSocket){
+      socket.to(sendUnderSocket).emit("msg-recieve", data.message)
+    }
+  })
+
+  socket.on("send-notification", (data)=>{
+    const sendUnderSocket = onlineUsers.get(data.to);
+    if(sendUnderSocket){
+      socket.to(sendUnderSocket).emit("notification-recieve",data.message)
+    }
+  })
+
+  // Add this new event listener for disconnect
+  socket.on("disconnect", () => {
+    console.log('Disconnected user:', socket.id);
+    // // Remove the disconnected user from the onlineUsers map
+    // for (const [userId, socketId] of onlineUsers.entries()) {
+    //   if (socketId === socket.id) {
+    //     onlineUsers.delete(userId);
+    //     console.log(`User ${userId} removed from online users`);
+    //     break;
+    //   }
+    // }
+  });
+})
